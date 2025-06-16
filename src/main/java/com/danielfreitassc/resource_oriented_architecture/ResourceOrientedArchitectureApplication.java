f022b1ae6daaa1cdc1f9a8ac67ffcf75255e7705
DanielFreitassc <danielfreitassc@gmail.com>
2025-06-16 20:17:17 -0300

    First commit

---
+++ src/main/java/com/danielfreitassc/resource_oriented_architecture/ResourceOrientedArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.danielfreitassc.resource_oriented_architecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceOrientedArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResourceOrientedArchitectureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danielfreitassc/resource_oriented_architecture/ResourceOrientedArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danielfreitassc.resource_oriented_architecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResourceOrientedArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
